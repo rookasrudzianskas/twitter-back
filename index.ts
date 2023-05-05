1524ed450302f35ceb79204802b107db449199b9
Rokas <rookas.rudzenskas@gmail.com>
2023-05-05 17:59:57 +0300

    🚀 feat(index.ts): add tweetRoutes to the app to enable tweet-related endpoints

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRoutes from "./routes/userRoutes";
+import tweetRoutes from "./routes/tweetRoutes";
 
 const app = express();
 app.use(express.json());
 app.use('/user', userRoutes);
+app.use('/tweet', tweetRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
