fb5a790b13f31ced7e236519c358c0fde41dda58
Rokas <rookas.rudzenskas@gmail.com>
2023-05-05 17:53:17 +0300

    🚀 feat(index.ts): add support for updating and deleting users 🐛 fix(index.ts): fix syntax error in delete user route definition

--- index.ts
+++ index.ts
@@ -23,4 +23,16 @@ app.get('/user/:id', (req, res) => {
   res.status(501).json({ message: `Not implemented ${id}` });
 });
 
+// Update the user
+app.put('/user/:id', (req, res) => {
+  const {id} = req.params;
+  res.status(501).json({ message: `Not implemented ${id}` });
+});
+
+// delete the user
+app.delete('/user/:id', (req, res) => {
+  const {id} = req.params;
+  res.status(501).json({ message: `Not implemented ${id}` });
+}
+
 app.listen(3000, () => console.log('Server running on port 3000'));
