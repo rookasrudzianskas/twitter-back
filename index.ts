9048a8753fa41f7b4aff50eb0ad97607c8690dbf
Rokas <rookas.rudzenskas@gmail.com>
2023-05-05 17:51:53 +0300

    ✨ feat(index.ts): add endpoints for listing all users and getting one user by id to improve user management capabilities

--- index.ts
+++ index.ts
@@ -7,8 +7,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// For the creating user
 app.post('/user', (req, res) => {
   res.status(501).json({ message: 'Not implemented' });
-})
+});
+
+// list all users
+app.get('/users', (req, res) => {
+  res.status(501).json({ message: 'Not implemented' });
+});
+
+// get a one user
+app.get('/user/:id', (req, res) => {
+  const {id} = req.params;
+  res.status(501).json({ message: `Not implemented ${id}` });
+});
 
 app.listen(3000, () => console.log('Server running on port 3000'));
