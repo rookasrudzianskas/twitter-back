f08715e66af2d6236ee547304b306eb03e40dec8
Rokas <rookas.rudzenskas@gmail.com>
2023-05-05 17:49:40 +0300

    🚀 feat(index.ts): add POST /user endpoint to the API to handle user creation requests

--- index.ts
+++ index.ts
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/user', (req, res) => {
+  res.status(501).json({ message: 'Not implemented' });
+})
+
 app.listen(3000, () => console.log('Server running on port 3000'));
