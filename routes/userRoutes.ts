8c0b2f91d1d2fad512116b76e443e1d7978c444e
Rokas <rookas.rudzenskas@gmail.com>
2023-05-05 17:55:38 +0300

    🆕 feat(userRoutes.ts): add userRoutes file to handle user-related routes 🔥 refactor(index.ts): remove user-related routes from index.ts and import them from userRoutes file to improve code organization and maintainability

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,38 +1,32 @@
-import express from 'express';
+import {Router} from 'express';
 
-const app = express();
-app.use(express.json());
+const router = Router();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 // For the creating user
-app.post('/user', (req, res) => {
+router.post('/user', (req, res) => {
   res.status(501).json({ message: 'Not implemented' });
 });
 
 // list all users
-app.get('/users', (req, res) => {
+router.get('/users', (req, res) => {
   res.status(501).json({ message: 'Not implemented' });
 });
 
 // get a one user
-app.get('/user/:id', (req, res) => {
+router.get('/user/:id', (req, res) => {
   const {id} = req.params;
   res.status(501).json({ message: `Not implemented ${id}` });
 });
 
 // Update the user
-app.put('/user/:id', (req, res) => {
+router.put('/user/:id', (req, res) => {
   const {id} = req.params;
   res.status(501).json({ message: `Not implemented ${id}` });
 });
 
 // delete the user
-app.delete('/user/:id', (req, res) => {
+router.delete('/user/:id', (req, res) => {
   const {id} = req.params;
   res.status(501).json({ message: `Not implemented ${id}` });
 });
-
-app.listen(3000, () => console.log('Server running on port 3000'));
