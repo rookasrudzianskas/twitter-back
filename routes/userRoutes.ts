ea82f07b327420ce7644978b0de36b16b6337150
Rokas <rookas.rudzenskas@gmail.com>
2023-05-06 09:56:23 +0300

    🚀 chore(package.json): update @prisma/client dependency to ^4.13.0 🔨 refactor(userRoutes.ts): use async/await and PrismaClient to create, list and get users 🔥 chore(userRoutes.ts): remove unused code and comments

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,22 +1,52 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const router = Router();
-
+const prisma = new PrismaClient();
 
 // For the creating user
-router.post('/', (req, res) => {
-  res.status(501).json({ message: 'Not implemented' });
+router.post('/', async (req, res) => {
+  const { email, name, username } = req.body;
+  // console.log(`email: ${email}, name: ${name}, username: ${username}`);
+  try {
+    const result = await prisma.user.create({
+      data: {
+        email,
+        name,
+        username,
+        bio: 'I am new on twitter',
+      },
+    });
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ error: 'Username and email should be unique' });
+  }
 });
 
 // list all users
-router.get('/', (req, res) => {
-  res.status(501).json({ message: 'Not implemented' });
+router.get('/', async (req, res) => {
+  const allUser = await prisma.user.findMany({
+    // select: {
+    //   id: true,
+    //   name: true,
+    //   image: true,
+    //   bio: true,
+    // },
+  });
+
+  res.json(allUser);
 });
 
 // get a one user
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const {id} = req.params;
-  res.status(501).json({ message: `Not implemented ${id}` });
+  const user = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+    // include: { tweets: true },
+  })
+  res.json(user);
 });
 
 // Update the user
