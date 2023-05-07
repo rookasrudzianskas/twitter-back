368b23b45e99c443da3abf4c972f878df1bfd476
Rokas <rookas.rudzenskas@gmail.com>
2023-05-07 14:48:00 +0300

    🚧 chore(authRoutes.ts): add authentication route to generate API token 🔒 chore(authRoutes.ts): add emailToken validation to authenticate route 🔒 chore(authRoutes.ts): add email ownership validation to authenticate route 🔒 chore(authRoutes.ts): add API token generation to authenticate route 🔒 chore(authRoutes.ts): add emailToken invalidation to authenticate route 🔒 chore(authRoutes.ts): add JWT token generation to authenticate route 🔒 chore(authRoutes.ts): return authToken in authenticate route response

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -62,4 +62,60 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// Validate the emailToken
+// Generate a long-lived JWT token
+router.post('/authenticate', async (req, res) => {
+  const { email, emailToken } = req.body;
+
+  const dbEmailToken = await prisma.token.findUnique({
+    where: {
+      emailToken,
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  // if (!dbEmailToken || !dbEmailToken.valid) {
+  //   return res.sendStatus(401);
+  // }
+  //
+  // if (dbEmailToken.expiration < new Date()) {
+  //   return res.status(401).json({ error: 'Token expired!' });
+  // }
+  //
+  // if (dbEmailToken?.user?.email !== email) {
+  //   return res.sendStatus(401);
+  // }
+
+  // Here we validated that the user is the owner of the email
+
+  // generate an API token
+  const expiration = new Date(
+    new Date().getTime() + AUTHENTICATION_EXPIRATION_HOURS * 60 * 60 * 1000
+  );
+  const apiToken = await prisma.token.create({
+    data: {
+      type: 'API',
+      expiration,
+      user: {
+        connect: {
+          email,
+        },
+      },
+    },
+  });
+
+  // // Invalidate the email
+  // await prisma.token.update({
+  //   where: { id: dbEmailToken.id },
+  //   data: { valid: false },
+  // });
+  //
+  // // generate the JWT token
+  // const authToken = generateAuthToken(apiToken.id);
+
+  // res.json({ authToken });
+});
+
 export default router;
