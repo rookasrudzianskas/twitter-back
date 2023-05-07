ef59e907703846803c3c0f11c81ad4d3e9788720
Rokas <rookas.rudzenskas@gmail.com>
2023-05-07 14:27:44 +0300

    🔒 chore(authRoutes.ts): add constants for email token expiration, authentication expiration and JWT secret to improve code readability ✨ feat(authRoutes.ts): add function to generate email token and function to generate authentication token 🚧 chore(authRoutes.ts): add skeleton code for login route to create user, generate email token and send it to their email

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,11 +1,33 @@
 import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 // import { sendEmailToken } from '../services/emailService';
+
+const EMAIL_TOKEN_EXPIRATION_MINUTES = 10;
+const AUTHENTICATION_EXPIRATION_HOURS = 12;
+const JWT_SECRET = process.env.JWT_SECRET || 'SUPER SECRET';
 
 const router = Router();
 const prisma = new PrismaClient();
 
+// Generate a random 8 digit number as the email token
+function generateEmailToken(): string {
+  return Math.floor(10000000 + Math.random() * 90000000).toString();
+}
 
+function generateAuthToken(tokenId: number): string {
+  const jwtPayload = { tokenId };
+
+  return jwt.sign(jwtPayload, JWT_SECRET, {
+    algorithm: 'HS256',
+    noTimestamp: true,
+  });
+}
+
+// Create a user, if it doesn't exist,
+// generate the emailToken and send it to their email
+router.post('/login', async (req, res) => {
+
+});
 
 export default router;
