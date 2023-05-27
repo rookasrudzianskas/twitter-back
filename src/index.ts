aec7e86245cf773aae5d41e474b0f6eeb0657e5b
Rokas <rookas.rudzenskas@gmail.com>
2023-05-27 15:40:52 +0300

    FIX

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use('/tweet', authenticateToken, tweetRoutes);
 app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello "updated" World!');
 });
 
 
